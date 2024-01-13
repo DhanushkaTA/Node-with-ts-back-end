95dc83efc9aa0620c29937096d6167a71c8f8ccf
DhanushkaTa <dhanushka999ta@gmail.com>
2024-01-13 12:53:21 +0530

    create seerver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello")
+// const express = require("express") //before ES5
+import express from 'express'
+
+//invoke the express
+const app= express();
+
+//start the server
+app.listen(8080,() =>{
+    console.log("server started on port 8080")
+})
