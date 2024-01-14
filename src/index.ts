08dd79030cf8230ba66256648c0fa745545bbc01
DhanushkaTa <dhanushka999ta@gmail.com>
2024-01-14 11:26:42 +0530

    connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 // const express = require("express") //before ES5
 import express from 'express'
 import bodyParser from "body-parser";
-
-//invoke the express
-const app= express();
-
-app.use(bodyParser.json());
+import * as mongoose from 'mongoose'
 
 interface User{
     id: string,
@@ -16,6 +12,32 @@ interface User{
 }
 
 let users:User[] = [];
+
+
+//invoke the express
+const app= express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/blog").then(r => {
+    console.log(`DB Connection Successfully`)
+}).catch(error => {
+    console.log(`DB Connection Error : ${error}`)
+})
+const db= mongoose.connection
+
+// db.on('error', (error) => {
+//     console.error(`DB Connection Error: ${error}`);
+// });
+//
+// db.on('open', () => {
+//     console.log('DB Connection Opened');
+// });
+//
+// db.on('close', () => {
+//     console.log('DB Connection Closed');
+// });
+
 
 //node-->routs (end-point)
 app.get('/user/all',(req :express.Request ,res :express.Response) =>{
