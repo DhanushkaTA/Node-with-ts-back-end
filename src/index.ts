aad8bc67e8e6a0a535eb023f0ea73582e09d8001
DhanushkaTa <dhanushka999ta@gmail.com>
2024-01-14 10:31:58 +0530

    add post method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
 // const express = require("express") //before ES5
 import express from 'express'
+import bodyParser from "body-parser";
 
 //invoke the express
 const app= express();
+
+app.use(bodyParser.json());
+
+interface User{
+    id: string,
+    username: string,
+    lName: string,
+    fName: string,
+    email: string
+}
+
+let users:User[] = [];
+
+//node-->routs (end-point)
+app.get('/user/all',(req :express.Request ,res :express.Response) =>{
+
+    res.send(users);
+})
+
+app.post('/user',(req, res)=>{
+
+    users.push(req.body);
+    res.send("OK!")
+})
+
+
 
 //start the server
 app.listen(8080,() =>{
